193e584d30ed8aa3260ff514d4ad1cb38774a8a2
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    toco: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_toco.mk
+++ lineage_toco.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -23,6 +23,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="toco-user 11 RKQ1.200826.002 V12.1.4.0.RFNMIXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/toco_global/toco:11/RKQ1.200826.002/V12.1.4.0.RFNMIXM:user/release-keys
+    BuildDesc="toco-user 12 RKQ1.210614.002 V13.0.6.0.SFNEUXM release-keys" \
+    BuildFingerprint=Xiaomi/toco_eea/toco:12/RKQ1.210614.002/V13.0.6.0.SFNEUXM:user/release-keys
